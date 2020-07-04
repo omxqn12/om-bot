452dab282236e4c193bdd96451c5d7b35a08e63f
omxqn12 <48726420+omxqn12@users.noreply.github.com>
2020-07-04 23:16:56 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "", // توكن بوتك
+  TOKEN: "NTI2ODY2MTEwMjk4MTkzOTIx.XwDTDQ.YAzJ5RMltfuN4WNHPGbPeSzl5_o", // توكن بوتك
   YT_API_KEY: "api ",
   prefix: "o",
   dev : "360469246108762113" //أيديك
